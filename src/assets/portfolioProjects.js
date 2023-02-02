81776d8f3fd1ff87e75d110c991468c29472442e
Josh <josh@ursacreative.ca>
2023-02-01 17:02:01 -0800

    Try to get sitemap

--- src/assets/portfolioProjects.js
+++ src/assets/portfolioProjects.js
@@ -1,13 +1,13 @@
 export default [
   {
     id: 'personal_website',
-    name: 'Personal Website',
+    name: 'Personal Website.',
     tags: ['Vue.', 'Node.', 'Tailwind.'],
     frontImage: 'personal_website/personal_website-slide1.webp',
   },
   {
     id: 'arcade_machine',
-    name: 'Arcade Machine',
+    name: 'Arcade Machine.',
     tags: ['RaspberryPi.', 'Godot.'],
     frontImage: 'arcade_machine/arcade_machine-slide1.webp',
   },
@@ -31,25 +31,25 @@ export default [
   },
   {
     id: 'larminay_vault',
-    name: 'Larminay Vault',
+    name: 'Larminay Vault.',
     tags: ['PHP.', 'JS.', 'HTML.', 'CSS.', 'MySQL.'],
     frontImage: 'larminay_vault/larminay_vault-slide1.webp',
   },
   {
     id: 'cell_simulator',
-    name: 'Cell Simulator',
+    name: 'Cell Simulator.',
     tags: ['JS.', 'HTML.', 'CSS.'],
     frontImage: 'cell_simulator/cell_simulator-slide1.webp',
   },
   {
     id: 'victoria_titan',
-    name: 'Victoria Titan',
+    name: 'Victoria Titan.',
     tags: ['JS.', 'HTML.', 'CSS.'],
     frontImage: 'victoria_titan/victoria_titan-slide1.webp',
   },
   {
     id: 'floor_check_report',
-    name: 'Floor Check Report',
+    name: 'Floor Check Report.',
     tags: ['PHP.', 'JS.', 'HTML.', 'CSS.'],
     frontImage: 'floor_check_report/floor_check_report-slide1.webp',
   },
